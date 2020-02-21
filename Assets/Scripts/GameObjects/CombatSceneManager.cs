ea9b5aa969b270497eda6c4c777c8fa8cfaf0b0e
Daniel Riissanen <daniel.riissanen@gmail.com>
2020-02-22 01:52:26 +0200

    Add transitions between scenes

---
+++ Assets/Scripts/GameObjects/CombatSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DarkestDimension {
+
+    public class CombatSceneManager : MonoBehaviour {
+        public void GoToNavigationScene() {
+            SceneManager.LoadSceneAsync("NavigationScene");
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameObjects/NavigationSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DarkestDimension {
+    public class NavigationSceneManager : MonoBehaviour {
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                SceneManager.LoadSceneAsync("CombatScene");
+            }
+        }
+    }
+
+}
